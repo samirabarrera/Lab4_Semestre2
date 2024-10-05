677ace5b5d71f7657beef2ec0f5856a5c2c276d4
Samira Barrera <164458958+samirabarrera@users.noreply.github.com>
2024-10-05 01:42:36 -0600

    agregando funciones

--- laboratorio4.js
+++ laboratorio4.js
@@ -17,8 +17,43 @@
     mostrar la información.
 
     Pruebe el script llamando a la secuencia:*/
+Object = images 
+      list: [
+      { title: "Mona Lisa", artist: "Leonardo da Vinci", date: 1503 },
+      { title: "La última cena", artist: "Leonardo da Vinci", date: 1495 },
+      { title: "La noche estrellada", artist: "Vincent van Gogh", date: 1889 },
+      { title: "El grito", artist: "Edvard Munch", date: 1893 },
+      { title: "El Guernica", artist: "Pablo Picasso", date: 1937 },
+      { title: "El beso", artist: "Gustav Klimt", date: 1907 },
+      { title: "La joven de la perla", artist: "Johannes Vermeer", date: 1665 },
+      { title: "El nacimiento de Venus", artist: "Sandro Botticelli", date: 1485 },
+      { title: "Las meninas", artist: "Diego Velázquez", date: 1656 },
+      { title: "La creación de Adán", artist: "Miguel Ángel", date: 1512 }
+    ]
 
-    
+function Image(title, artist, date) {
+    this.title = title;
+    this.artist = artist;
+    this.date = date;
+}
+
+const images = {
+      add: function (title, artist, date) {
+          const newImage = new Image(title, artist, date);
+          this.list.push(newImage); 
+      },
+
+      delete: function (title, artist, date) {
+            const newImage = new Image(title, artist, date);
+            this.list.push(newImage); 
+        },
+
+      edit: function(title, artist, date) {
+            const newImage = new Image(title, artist, date);
+            this.list.push(newImage); 
+        },
+}
+
 
     images.add('Mona Lisa', 'Leonardo da Vinci', 1503);
     images.add('The Last Supper', 'Leonardo da Vinci', 1495);
